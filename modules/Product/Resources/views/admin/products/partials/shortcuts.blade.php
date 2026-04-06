596eed74d8cd7358570f335050b7d8b6950dec99
zaibibaloch <aurangzeb@codencoffee.com>
2026-04-06 21:01:31 +0500

    condition applied

--- modules/Product/Resources/views/admin/products/partials/shortcuts.blade.php
+++ modules/Product/Resources/views/admin/products/partials/shortcuts.blade.php
@@ -2,8 +2,10 @@
     <dl class="dl-horizontal">
         <dt><code>b</code></dt>
         <dd>{{ trans('admin::admin.shortcuts.back_to_index', ['name' => trans('product::products.product')]) }}</dd>
-        <dt><code>d</code></dt>
-        <dd>Duplicate this product</dd>
+        @if (isset($product) && $product?->id)
+            <dt><code>d</code></dt>
+            <dd>Duplicate this product</dd>
+        @endif
     </dl>
 @endpush
 
@@ -11,7 +13,9 @@
     <script type="module">
         keypressAction([
             { key: 'b', route: "{{ route('admin.products.index') }}" },
-            { key: 'd', route: "{{ route('admin.products.duplicate', $product->id) }}" },
+            @if (isset($product) && $product?->id)
+                { key: 'd', route: "{{ route('admin.products.duplicate', $product->id) }}" },
+            @endif
         ]);
     </script>
 @endpush
